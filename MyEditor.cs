0f851a14eda068409a26561b73fd759ac08e569b
ShuxinWen <33890591+ShuxinWen@users.noreply.github.com>
2021-08-22 23:36:27 +0800

    Add files via upload

---
+++ MyEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(EditorTest))]
+[ExecuteInEditMode]
+public class MyEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        EditorTest editorTest = (EditorTest)target;
+        editorTest.rectValue = EditorGUILayout.RectField("窗口坐标设置", editorTest.rectValue);
+        editorTest.texture = EditorGUILayout.ObjectField("选择贴图", editorTest.texture, typeof(Texture), true) as Texture;
+    }
+}
